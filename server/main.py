3278a6a3d18003e32bdbcbe2793df79099daffd1
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2017-03-13 16:43:00 +0100

    Added some train funcionality, need to test

--- server/main.py
+++ server/main.py
@@ -2,53 +2,145 @@ from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import Cookie
 import zlib
+import graphlab
+import os
+import tempfile
+
+import models_config
 
 # HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
 
- 
-  # GET
-  def do_GET(self):
-    # Send response status code
-    self.send_response(200)
- 
-    # Send headers
-    self.send_header('Content-type','text/html')
-    self.end_headers()
- 
-    # Send message back to client
-    message = "Hello world!"
-    # Write content as utf-8 data
-    self.wfile.write(bytes(message, "utf8"))
-    return
 
-  def do_POST(self):
-    gz_blob = self.rfile.read(int(self.headers['Content-Length']))
-    print(self.path)
-    c = Cookie.SimpleCookie()
-    c.load(self.headers.get("Cookie"))
-    print(c['user'].value)
-    self.send_response(200)
-    self.send_header('Content-type','text/html')
-    self.end_headers()
-
-    message = zlib.decompress(gz_blob, 16+zlib.MAX_WBITS)
+    def get_msg(self):
+        gz_blob = self.rfile.read(int(self.headers['Content-Length']))
+        message = zlib.decompress(gz_blob, 16+zlib.MAX_WBITS)
+        return message
 
 
-    print(message)
+    def save_msg_to_file(self, path):
+        message = self.get_msg()
+        if os.path.isfile(path): #user already has data
+            message = "\n".join(message.split("\n")[1:]) + "\n" #remove csv headers
 
-    return
- 
+        with open(path, 'a') as f:
+            f.write(message)
+
+    def update_taps(self):
+        path = "data/" + self.user + "_taps.csv"
+        save_msg_to_file(path)
+
+    def update_apps(self):
+        path = "data/" + self.user + "_apps.csv"
+        save_msg_to_file(path)
+
+    def train_taps(self):
+        path = "data/" + self.user + "_taps.csv"
+        user_taps_sf = graphlab.SFrame.read_csv()
+        print user_taps_sf
+
+        not_noise = user_taps_sf[user_taps_sf['noise'] == 0] #check
+        swipes = not_noise[not_noise['type'] == "SWIPE"]
+        touches = not_noise[not_noise['type'] == "TOUCH"]
+
+        noise_model = graphlab.boosted_trees_classifier.create(user_taps_sf,
+                                                            target="noise",
+                                                            features=models_config.features,
+                                                            max_iterations = models_config.noise_max_iterations,
+                                                            max_depth = models_config.noise_max_depth)
+       type_model = graphlab.boosted_trees_classifier.create(not_noise,
+                                                            target="type",
+                                                            features=models_config.features,
+                                                            max_iterations = models_config.type_max_iterations,
+                                                            max_depth = models_config.type_max_depth)
+
+        swipe_model = graphlab.boosted_trees_classifier.create(swipes,
+                                                             target="action",
+                                                             features=models_config.features,
+                                                             max_iterations = models_config.swipe_max_iterations,
+                                                             max_depth = models_config.swipe_max_depth)
+
+        touch_model = graphlab.boosted_trees_classifier.create(touches,
+                                                             target="action",
+                                                             features=models_config.features,
+                                                             max_iterations = models_config.touch_max_iterations,
+                                                             max_depth = models_config.touch_max_depth)
+
+        noise_model.save("models/" + self.user + "noise_model")
+        type_model.save("models/" + self.user + "type_model")
+        swipe_model.save("models/" + self.user + "swipe_model")
+        touch_model.save("models/" + self.user + "touch_model")
+
+
+
+
+    def predict_taps(self):
+        path = "/tmp/" + self.user + "_temp_taps.csv"
+        save_msg_to_file(path)
+        taps_sf = graphlab.SFrame.read_csv(path)
+        noise_model = graphlab.load_model("models/" + self.user + "noise_model")
+        type_model = graphlab.load_model("models/" + self.user + "type_model")
+        touch_model = graphlab.load_model("models/" + self.user + "touch_model")
+        swipe_model = graphlab.load_model("models/" + self.user + "swipe_model")
+
+        result = []
+
+        for tap_sf in taps_sf:
+            if noise_model.predict(tap_sf):
+                result.append("NOISE")
+                continue
+            if type_model.predict(tap_sf) == "SWIPE":
+                result.append(swipe_model.predict(tap_sf))
+            else:
+                result.append(touch_model.predict(tap_sf))
+
+        print result
+
+    def do_POST(self):
+
+        c = Cookie.SimpleCookie()
+        try:
+            c.load(self.headers.get("Cookie"))
+            self.user = c['user'].value
+            if not self.user.isdigit():
+                raise Exception("Non numeric user")
+        except:
+            self.send_response(403)
+            self.end_headers()
+            return
+
+
+        if self.path == "/train_taps":
+            self.train_taps()
+        elif self.path == "/train_apps":
+            self.train_apps()
+        if self.path == "/update_taps":
+            self.update_taps()
+        elif self.path == "/update_apps":
+            self.update_apps()
+        elif self.path == "/predict_taps":
+            self.predict_taps()
+        elif self.path == "/predict_apps":
+            self.predict_apps()
+        else:
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        return
+
+
 def run():
   print('starting server...')
- 
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+
   server_address = ('0.0.0.0', 8081)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  httpd = HTTPServer(server_address, HTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
- 
- 
-run()
+
+if __name__ == "__main__":
+    run()
---
+++ server/models_config.py
@@ -0,0 +1,60 @@
+features = [
+    "n_accel",
+
+    "accel_x_mean",
+    "accel_x_median",
+    "accel_x_var",
+    "accel_x_skewness",
+    "accel_x_kurtosis",
+    "accel_x_diff",
+
+    "accel_y_mean",
+    "accel_y_median",
+    "accel_y_skewness",
+    "accel_y_kurtosis",
+    "accel_y_var",
+    "accel_y_diff",
+
+    "accel_z_mean",
+    "accel_z_var",
+    "accel_z_median",
+    "accel_z_skewness",
+    "accel_z_kurtosis",
+    "accel_z_diff",
+
+    "n_gyro",
+
+    "gyro_x_mean",
+    "gyro_x_median",
+    "gyro_x_var",
+    "gyro_x_skewness",
+    "gyro_x_kurtosis",
+    "gyro_x_diff",
+
+    "gyro_y_mean",
+    "gyro_y_median",
+    "gyro_y_var",
+    "gyro_y_skewness",
+    "gyro_y_kurtosis",
+    "gyro_y_diff",
+
+    "gyro_z_mean",
+    "gyro_z_median",
+    "gyro_z_var",
+    "gyro_z_skewness",
+    "gyro_z_kurtosis",
+    "gyro_z_diff",
+
+]
+
+noise_max_iterations = 640
+noise_max_depth = 4
+
+type_max_iterations = 160
+type_max_depth = 6
+
+swipe_max_iterations = 40
+swipe_max_depth = 6
+
+touch_max_iterations = 640
+touch_max_depth = 2
