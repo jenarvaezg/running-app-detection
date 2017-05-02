55865e13a8b4f41ce89a7ba75e1f3d5c655949e4
Jose Enrique Narváez <jenarvaezg@gmail.com>
2017-05-02 21:08:37 +0200

    Added some locks and a part of app training

--- server/main.py
+++ server/main.py
@@ -29,7 +29,7 @@ class Predictor():
         self.type_model = models[self.user]['type'] 
         self.touch_model = models[self.user]['touch']
         self.swipe_model = models[self.user]['swipe']
-        self.lock = threading.Lock
+        self.lock = threading.Lock()
         self.lock.acquire()
 
     def get_prediction(self):
@@ -95,6 +95,9 @@ class Predictor():
 class RequestHandler(BaseHTTPRequestHandler):
 
     
+    taps_file_mutex = threading.Lock()
+    apps_file_mutex = threading.Lock()
+
     def get_msg(self, compressed=False):
         blob = self.rfile.read(int(self.headers['Content-Length']))
 	if not compressed:
@@ -107,9 +110,11 @@ class RequestHandler(BaseHTTPRequestHandler):
         message = self.get_msg(compressed)
         if os.path.isfile(path): #user already has data
             message = "\n".join(message.split("\n")[1:]) + "\n" #remove csv headers
-
+        
         with open(path, 'a') as f:
+            RequestHandler.taps_file_mutex.acquire()
             f.write(message)
+            RequestHandler.taps_file_mutex.release()
 
     def update_taps(self):
         path = "data/" + self.user + "_taps.csv"
@@ -117,7 +122,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def update_apps(self):
         path = "data/" + self.user + "_apps.csv"
-        self.save_msg_to_file(path)
+        with open(path, 'a') as f:
+            RequestHandler.apps_file_mutex.acquire()
+            for line in self.rfile:
+                print line
+                f.write(line)
+            RequestHandler.apps_file_mutex.release()
 
     def train_taps(self):
         path = "data/" + self.user + "_taps.csv"
@@ -157,6 +167,37 @@ class RequestHandler(BaseHTTPRequestHandler):
         touch_model.save("models/" + self.user + "_touch_model")
     	load_models()
 
+    def train_apps(self):
+        # all this might change, I might want to use the same code as the predictor up there 
+        path = "data/" + self.user + "_taps.csv"
+        user_taps_sf = graphlab.SFrame.read_csv(path)
+    
+        noise_model = models[self.user]['noise']
+        type_model = models[self.user]['type']
+        touch_model = models[self.user]['touch']
+        swipe_model = models[self.user]['swipe']
+
+        for sf in user_taps_sf:
+            if noise_model.predict(sf) == 1:
+                sf['word'] = "NOISE"
+                continue
+            if type_model.predict(sf) == "SWIPE":
+                sf['word'] = swipe_model.predict(sf)
+            else:
+                sf['word'] = touch_model.predict(sf)
+        return
+
+        for app in sf['app'].unique():
+            compressed = compress(user_taps_sf[user_taps_sf['app' == app]])
+            word_count = graphlab.text_analytics.count_words(compressed)
+            
+
+        corups_words = graphlab.SFrame([
+        compressed_sf = compress(user_taps_sf) #this should be and sf with only words and app
+        # somewhere, pass it to bag of words
+        comppressed_sf['tfidf'] = graphlab
+
+
 
 
     def predict_taps(self):
@@ -193,11 +234,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         elif self.path == "/update_apps":
             self.update_apps()
         elif self.path == "/predict_taps":
-	    self.send_response(200)
-	    self.send_header('Content-type', 'text/html')
-	    self.end_headers()
             self.predict_taps()
-	    return
         elif self.path == "/predict_apps":
             self.predict_apps()
         else:
